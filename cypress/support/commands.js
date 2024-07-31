3e7a226e8e15d36a666603464b8158a0e72515e8
conversayShawn <shawn.harris3510@gmail.com>
2024-07-31 09:35:36 -0400

    echo run_id, add condition to custom command

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -2,11 +2,16 @@ Cypress.Commands.add('logHeaders', (testName) => {
     const githubRunId = Cypress.env('GITHUB_RUN_ID');
     // const githubToken = "Hello";
   
-    const headers = {
-      'UNIQUE-IDENTIFIER': `${githubRunId} - ${testName}`,
-    };
+    // Check if GH run_id is defined
+    if (!githubRunId) {
+      console.error('GITHUB_RUN_ID is undefined');
+    } else {
+      const headers = {
+        'UNIQUE-IDENTIFIER': `${githubRunId} - ${testName}`,
+      };
   
     // Log headers to the console and Cypress log
     console.log('Request Headers:', headers);
     cy.log(`Request Headers: ${JSON.stringify(headers)}`);
+    }
   });
